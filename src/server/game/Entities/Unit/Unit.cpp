Krigsgaldrnet/TrinityCore-3.3.5

commit bcc5a308502774b61cef8a19534658697401ca46

Core/Spells: do not apply unit_state_casting for instant cast spells.

src/server/game/Entities/Unit/Unit.cpp
@@ -2893,7 +2893,8 @@ void Unit::SetCurrentCastedSpell(Spell* pSpell)
                     InterruptSpell(CURRENT_AUTOREPEAT_SPELL);
                 m_AutoRepeatFirstCast = true;
             }
-            AddUnitState(UNIT_STATE_CASTING);
+            if (m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->CalcCastTime(this) > 0 && IsNonMeleeSpellCasted(false, false, true))
+                AddUnitState(UNIT_STATE_CASTING);
         } break;
 
         case CURRENT_CHANNELED_SPELL:

commit dd8d199fde606720b7fe2ceea884fe406c7f9363

Core/Spells: fix crash introduced in bcc5a308502774b61cef8a19534658697401ca46 (thx vincent-michael)

src/server/game/Entities/Unit/Unit.cpp
@@ -2893,7 +2893,7 @@ void Unit::SetCurrentCastedSpell(Spell* pSpell)
                     InterruptSpell(CURRENT_AUTOREPEAT_SPELL);
                 m_AutoRepeatFirstCast = true;
             }
-            if (m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->CalcCastTime(this) > 0 && IsNonMeleeSpellCasted(false, false, true))
+            if ((m_currentSpells[CURRENT_GENERIC_SPELL] && m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->CalcCastTime(this) > 0) && IsNonMeleeSpellCasted(false, false, true))
                 AddUnitState(UNIT_STATE_CASTING);
         } break;
 