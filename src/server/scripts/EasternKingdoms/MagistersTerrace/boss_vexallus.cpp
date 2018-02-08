Krigsgaldrnet/TrinityCore-3.3.5

commit d8748312dd8585f4e99ca696284926bc92b62789

Core/Scripts: Fixed possible crashs related with JustDied

src/server/scripts/EasternKingdoms/MagistersTerrace/boss_vexallus.cpp
@@ -208,7 +208,8 @@ class npc_pure_energy : public CreatureScript
 
             void JustDied(Unit* killer) override
             {
-                killer->CastSpell(killer, SPELL_ENERGY_FEEDBACK, true);
+                if (killer)
+                    killer->CastSpell(killer, SPELL_ENERGY_FEEDBACK, true);
                 me->RemoveAurasDueToSpell(SPELL_PURE_ENERGY_PASSIVE);
             }
         };

src/server/scripts/EasternKingdoms/ScarletEnclave/chapter2.cpp
@@ -550,7 +550,8 @@ public:
         void JustDied(Unit* killer) override
         {
             Talk(SAY_VALROTH_DEATH);
-            killer->CastSpell(me, SPELL_SUMMON_VALROTH_REMAINS, true);
+			if (killer)
+                killer->CastSpell(me, SPELL_SUMMON_VALROTH_REMAINS, true);
         }
     };
 

src/server/scripts/Northrend/zone_howling_fjord.cpp
@@ -638,10 +638,10 @@ public:
 
         void JustDied(Unit* killer) override
         {
+			if (!killer || killer->GetTypeId() != TYPEID_PLAYER)
+				return;
+			
             Player* player = killer->ToPlayer();
-
-            if (!player)
-                return;
 
             if (roll_chance_i(20))
             {

src/server/scripts/Outland/HellfireCitadel/BloodFurnace/boss_kelidan_the_breaker.cpp
@@ -312,6 +312,9 @@ class npc_shadowmoon_channeler : public CreatureScript
 
             void JustDied(Unit* killer) override
             {
+			   if (!killer)
+				   return;
+			   
                if (Creature* Kelidan = me->FindNearestCreature(ENTRY_KELIDAN, 100))
                    ENSURE_AI(boss_kelidan_the_breaker::boss_kelidan_the_breakerAI, Kelidan->AI())->ChannelerDied(killer);
             }

src/server/scripts/Outland/HellfireCitadel/HellfireRamparts/boss_vazruden_the_herald.cpp
@@ -487,6 +487,9 @@ class npc_hellfire_sentry : public CreatureScript
 
             void JustDied(Unit* killer) override
             {
+				if (!killer)
+					return;
+				
                 if (Creature* herald = me->FindNearestCreature(NPC_VAZRUDEN_HERALD, 150))
                     ENSURE_AI(boss_vazruden_the_herald::boss_vazruden_the_heraldAI, herald->AI())->SentryDownBy(killer);
             }

src/server/scripts/Outland/zone_shadowmoon_valley.cpp
@@ -973,6 +973,9 @@ public:
 
         void JustDied(Unit* killer) override
         {
+			if (!killer)
+				return;
+			
             switch (killer->GetTypeId())
             {
                 case TYPEID_UNIT:

src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_nefarian.cpp
@@ -436,7 +436,7 @@ public:
             Talk(SAY_RANDOM);
         }
 
-        void JustDied(Unit* /*Killer*/) override
+        void JustDied(Unit* /*killer*/) override
         {
             _JustDied();
             Talk(SAY_DEATH);

src/server/scripts/EasternKingdoms/Karazhan/boss_midnight.cpp
@@ -199,13 +199,13 @@ public:
             }
         }
 
-        void JustDied(Unit* killer) override
+        void JustDied(Unit* /*killer*/) override
         {
             Talk(SAY_DEATH);
             if (Unit* midnight = ObjectAccessor::GetUnit(*me, _midnightGUID))
                 midnight->KillSelf();
 
-            BossAI::JustDied(killer);
+            _JustDied();
         }
 
         void SetGUID(ObjectGuid const& guid, int32 id) override

commit 853da18cf5c18774d6bebce05b6f39fbc024a47e

Core/Scripts: Fixed tabs issues introduced in d8748312dd8585f4e99ca696284926bc92b62789

src/server/scripts/EasternKingdoms/MagistersTerrace/boss_vexallus.cpp
@@ -83,11 +83,6 @@ class boss_vexallus : public CreatureScript
                 Talk(SAY_KILL);
             }
 
-            void JustDied(Unit* /*killer*/) override
-            {
-                _JustDied();
-            }
-
             void JustEngagedWith(Unit* /*who*/) override
             {
                 Talk(SAY_AGGRO);

src/server/scripts/EasternKingdoms/ScarletEnclave/chapter2.cpp
@@ -550,7 +550,7 @@ public:
         void JustDied(Unit* killer) override
         {
             Talk(SAY_VALROTH_DEATH);
-			if (killer)
+            if (killer)
                 killer->CastSpell(me, SPELL_SUMMON_VALROTH_REMAINS, true);
         }
     };

src/server/scripts/Northrend/zone_howling_fjord.cpp
@@ -638,9 +638,9 @@ public:
 
         void JustDied(Unit* killer) override
         {
-			if (!killer || killer->GetTypeId() != TYPEID_PLAYER)
-				return;
-			
+            if (!killer || killer->GetTypeId() != TYPEID_PLAYER)
+                return;
+
             Player* player = killer->ToPlayer();
 
             if (roll_chance_i(20))

src/server/scripts/Outland/HellfireCitadel/BloodFurnace/boss_kelidan_the_breaker.cpp
@@ -312,11 +312,11 @@ class npc_shadowmoon_channeler : public CreatureScript
 
             void JustDied(Unit* killer) override
             {
-			   if (!killer)
-				   return;
-			   
-               if (Creature* Kelidan = me->FindNearestCreature(ENTRY_KELIDAN, 100))
-                   ENSURE_AI(boss_kelidan_the_breaker::boss_kelidan_the_breakerAI, Kelidan->AI())->ChannelerDied(killer);
+                if (!killer)
+                    return;
+
+                if (Creature* Kelidan = me->FindNearestCreature(ENTRY_KELIDAN, 100))
+                    ENSURE_AI(boss_kelidan_the_breaker::boss_kelidan_the_breakerAI, Kelidan->AI())->ChannelerDied(killer);
             }
 
             void UpdateAI(uint32 diff) override

src/server/scripts/Outland/HellfireCitadel/HellfireRamparts/boss_vazruden_the_herald.cpp
@@ -487,9 +487,9 @@ class npc_hellfire_sentry : public CreatureScript
 
             void JustDied(Unit* killer) override
             {
-				if (!killer)
-					return;
-				
+                if (!killer)
+                    return;
+
                 if (Creature* herald = me->FindNearestCreature(NPC_VAZRUDEN_HERALD, 150))
                     ENSURE_AI(boss_vazruden_the_herald::boss_vazruden_the_heraldAI, herald->AI())->SentryDownBy(killer);
             }

src/server/scripts/Outland/zone_shadowmoon_valley.cpp
@@ -973,9 +973,9 @@ public:
 
         void JustDied(Unit* killer) override
         {
-			if (!killer)
-				return;
-			
+            if (!killer)
+                return;
+
             switch (killer->GetTypeId())
             {
                 case TYPEID_UNIT: