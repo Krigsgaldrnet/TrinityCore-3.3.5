Krigsgaldrnet/TrinityCore-3.3.5

commit d86c693c312bfde05be219ac3be98e2952732a89

Core/Authserver: Authserver cleanup (PR#25093)
- Fix a handful of 1/256 bugs with most significant byte zero in BigNumber
- Get rid of (most of) the C-style arrays in authserver
- CryptoRandom as a unified source for cryptographic randomness
- Bring our other crypto APIs into 2020
- BigNumber usability improvements
- Authserver is now actually readable as a result of all of the above

src/server/game/Warden/WardenWin.cpp
@@ -15,34 +15,35 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Cryptography/HMACSHA1.h"
-#include "Cryptography/WardenKeyGeneration.h"
+#include "WardenWin.h"
 #include "Common.h"
-#include "WorldPacket.h"
-#include "WorldSession.h"
+#include "ByteBuffer.h"
+#include "CryptoRandom.h"
+#include "DatabaseEnv.h"
+#include "GameTime.h"
+#include "HMAC.h"
 #include "Log.h"
 #include "Opcodes.h"
-#include "ByteBuffer.h"
-#include "Database/DatabaseEnv.h"
-#include "GameTime.h"
-#include "World.h"
 #include "Player.h"
+#include "Random.h"
+#include "SessionKeyGenerator.h"
 #include "Util.h"
-#include "WardenWin.h"
 #include "WardenModuleWin.h"
 #include "WardenCheckMgr.h"
-#include "Random.h"
+#include "World.h"
+#include "WorldPacket.h"
+#include "WorldSession.h"
 #include <openssl/md5.h>
 
 WardenWin::WardenWin() : Warden(), _serverTicks(0) {}
 
 WardenWin::~WardenWin() { }
 
-void WardenWin::Init(WorldSession* session, BigNumber* k)
+void WardenWin::Init(WorldSession* session, std::array<uint8, 40> const& K)
 {
     _session = session;
     // Generate Warden Key
-    SHA1Randx WK(k->AsByteArray().get(), k->GetNumBytes());
+    SessionKeyGenerator<Trinity::Crypto::SHA1> WK(K);
     WK.Generate(_inputKey, 16);
     WK.Generate(_outputKey, 16);
 
@@ -262,7 +263,8 @@ void WardenWin::RequestData()
             case PAGE_CHECK_A:
             case PAGE_CHECK_B:
             {
-                buff.append(wd->Data.AsByteArray(0, false).get(), wd->Data.GetNumBytes());
+                std::vector<uint8> data = wd->Data.ToByteVector(0, false);
+                buff.append(data.data(), data.size());
                 buff << uint32(wd->Address);
                 buff << uint8(wd->Length);
                 break;
@@ -275,18 +277,16 @@ void WardenWin::RequestData()
             }
             case DRIVER_CHECK:
             {
-                buff.append(wd->Data.AsByteArray(0, false).get(), wd->Data.GetNumBytes());
+                std::vector<uint8> data = wd->Data.ToByteVector(0, false);
+                buff.append(data.data(), data.size());
                 buff << uint8(index++);
                 break;
             }
             case MODULE_CHECK:
             {
-                uint32 seed = rand32();
-                buff << uint32(seed);
-                HmacHash hmac(4, (uint8*)&seed);
-                hmac.UpdateData(wd->Str);
-                hmac.Finalize();
-                buff.append(hmac.GetDigest(), hmac.GetLength());
+                std::array<uint8, 4> seed = Trinity::Crypto::GetRandomBytes<4>();
+                buff.append(seed);
+                buff.append(Trinity::Crypto::HMAC_SHA1::GetDigestOf(seed, wd->Str));
                 break;
             }
             /*case PROC_CHECK:
@@ -391,7 +391,8 @@ void WardenWin::HandleData(ByteBuffer &buff)
                     continue;
                 }
 
-                if (memcmp(buff.contents() + buff.rpos(), rs->Result.AsByteArray(0, false).get(), rd->Length) != 0)
+                std::vector<uint8> result = rs->Result.ToByteVector();
+                if (memcmp(buff.contents() + buff.rpos(), result.data(), rd->Length) != 0)
                 {
                     TC_LOG_DEBUG("warden", "RESULT MEM_CHECK fail CheckId %u account Id %u", *itr, _session->GetAccountId());
                     checkFailed = *itr;
@@ -470,7 +471,7 @@ void WardenWin::HandleData(ByteBuffer &buff)
                     continue;
                 }
 
-                if (memcmp(buff.contents() + buff.rpos(), rs->Result.AsByteArray(0, false).get(), 20) != 0) // SHA1
+                if (memcmp(buff.contents() + buff.rpos(), rs->Result.ToByteArray<20>(false).data(), 20) != 0) // SHA1
                 {
                     TC_LOG_DEBUG("warden", "RESULT MPQ_CHECK fail, CheckId %u account Id %u", *itr, _session->GetAccountId());
                     checkFailed = *itr;

src/common/Cryptography/AES.cpp
@@ -37,7 +37,7 @@ void Trinity::Crypto::AES::Init(Key const& key)
     ASSERT(status);
 }
 
-bool Trinity::Crypto::AES::Process(IV const& iv, uint8* data, std::size_t length, Tag& tag)
+bool Trinity::Crypto::AES::Process(IV const& iv, uint8* data, size_t length, Tag& tag)
 {
     ASSERT(length <= std::numeric_limits<int>::max());
     int len = static_cast<int>(length);

src/common/Cryptography/AES.h
@@ -22,33 +22,30 @@
 #include <array>
 #include <openssl/evp.h>
 
-namespace Trinity
+namespace Trinity::Crypto
 {
-namespace Crypto
-{
-class TC_COMMON_API AES
-{
-public:
-    static constexpr std::size_t IV_SIZE_BYTES = 12;
-    static constexpr std::size_t KEY_SIZE_BYTES = 16;
-    static constexpr std::size_t TAG_SIZE_BYTES = 12;
+    class TC_COMMON_API AES
+    {
+    public:
+        static constexpr size_t IV_SIZE_BYTES = 12;
+        static constexpr size_t KEY_SIZE_BYTES = 16;
+        static constexpr size_t TAG_SIZE_BYTES = 12;
 
-    using IV = std::array<uint8, IV_SIZE_BYTES>;
-    using Key = std::array<uint8, KEY_SIZE_BYTES>;
-    using Tag = uint8[TAG_SIZE_BYTES];
+        using IV = std::array<uint8, IV_SIZE_BYTES>;
+        using Key = std::array<uint8, KEY_SIZE_BYTES>;
+        using Tag = uint8[TAG_SIZE_BYTES];
 
-    AES(bool encrypting);
-    ~AES();
+        AES(bool encrypting);
+        ~AES();
 
-    void Init(Key const& key);
+        void Init(Key const& key);
 
-    bool Process(IV const& iv, uint8* data, std::size_t length, Tag& tag);
+        bool Process(IV const& iv, uint8* data, size_t length, Tag& tag);
 
-private:
-    EVP_CIPHER_CTX* _ctx;
-    bool _encrypting;
-};
-}
+    private:
+        EVP_CIPHER_CTX* _ctx;
+        bool _encrypting;
+    };
 }
 
 #endif // Trinity_AES_h__

src/common/Cryptography/ARC4.cpp
@@ -16,35 +16,33 @@
  */
 
 #include "ARC4.h"
+#include "Errors.h"
 
-ARC4::ARC4(uint32 len) : m_ctx(EVP_CIPHER_CTX_new())
+Trinity::Crypto::ARC4::ARC4() : _ctx(EVP_CIPHER_CTX_new())
 {
-    EVP_CIPHER_CTX_init(m_ctx);
-    EVP_EncryptInit_ex(m_ctx, EVP_rc4(), nullptr, nullptr, nullptr);
-    EVP_CIPHER_CTX_set_key_length(m_ctx, len);
+    EVP_CIPHER_CTX_init(_ctx);
+    int result = EVP_EncryptInit_ex(_ctx, EVP_rc4(), nullptr, nullptr, nullptr);
+    ASSERT(result == 1);
 }
 
-ARC4::ARC4(uint8* seed, uint32 len) : m_ctx(EVP_CIPHER_CTX_new())
+Trinity::Crypto::ARC4::~ARC4()
 {
-    EVP_CIPHER_CTX_init(m_ctx);
-    EVP_EncryptInit_ex(m_ctx, EVP_rc4(), nullptr, nullptr, nullptr);
-    EVP_CIPHER_CTX_set_key_length(m_ctx, len);
-    EVP_EncryptInit_ex(m_ctx, nullptr, nullptr, seed, nullptr);
+    EVP_CIPHER_CTX_free(_ctx);
 }
 
-ARC4::~ARC4()
+void Trinity::Crypto::ARC4::Init(uint8 const* seed, size_t len)
 {
-    EVP_CIPHER_CTX_free(m_ctx);
+    int result1 = EVP_CIPHER_CTX_set_key_length(_ctx, len);
+    ASSERT(result1 == 1);
+    int result2 = EVP_EncryptInit_ex(_ctx, nullptr, nullptr, seed, nullptr);
+    ASSERT(result2 == 1);
 }
 
-void ARC4::Init(uint8* seed)
-{
-    EVP_EncryptInit_ex(m_ctx, nullptr, nullptr, seed, nullptr);
-}
-
-void ARC4::UpdateData(int len, uint8* data)
+void Trinity::Crypto::ARC4::UpdateData(uint8* data, size_t len)
 {
     int outlen = 0;
-    EVP_EncryptUpdate(m_ctx, data, &outlen, data, len);
-    EVP_EncryptFinal_ex(m_ctx, data, &outlen);
+    int result1 = EVP_EncryptUpdate(_ctx, data, &outlen, data, len);
+    ASSERT(result1 == 1);
+    int result2 = EVP_EncryptFinal_ex(_ctx, data, &outlen);
+    ASSERT(result2 == 1);
 }

src/common/Cryptography/ARC4.h
@@ -19,18 +19,27 @@
 #define _AUTH_SARC4_H
 
 #include "Define.h"
+#include <array>
 #include <openssl/evp.h>
 
-class TC_COMMON_API ARC4
+namespace Trinity::Crypto
 {
-    public:
-        ARC4(uint32 len);
-        ARC4(uint8* seed, uint32 len);
-        ~ARC4();
-        void Init(uint8* seed);
-        void UpdateData(int len, uint8* data);
-    private:
-        EVP_CIPHER_CTX* m_ctx;
-};
+    class TC_COMMON_API ARC4
+    {
+        public:
+            ARC4();
+            ~ARC4();
+
+            void Init(uint8 const* seed, size_t len);
+            template <typename Container>
+            void Init(Container const& c) { Init(std::data(c), std::size(c)); }
+
+            void UpdateData(uint8* data, size_t len);
+            template <typename Container>
+            void UpdateData(Container& c) { UpdateData(std::data(c), std::size(c)); }
+        private:
+            EVP_CIPHER_CTX* _ctx;
+    };
+}
 
 #endif

src/common/Cryptography/Argon2.cpp
@@ -21,12 +21,13 @@
 /*static*/ Optional<std::string> Trinity::Crypto::Argon2::Hash(std::string const& password, BigNumber const& salt, uint32 nIterations, uint32 kibMemoryCost)
 {
     char buf[ENCODED_HASH_LEN];
+    std::vector<uint8> saltBytes = salt.ToByteVector();
     int status = argon2id_hash_encoded(
         nIterations,
         kibMemoryCost,
         PARALLELISM,
         password.c_str(), password.length(),
-        salt.AsByteArray().get(), salt.GetNumBytes(),
+        saltBytes.data(), saltBytes.size(),
         HASH_LEN, buf, ENCODED_HASH_LEN
     );
 

src/common/Cryptography/Argon2.h
@@ -23,22 +23,19 @@
 #include "Optional.h"
 #include <string>
 
-namespace Trinity
+namespace Trinity::Crypto
 {
-namespace Crypto
-{
-struct TC_COMMON_API Argon2
-{
-    static constexpr uint32 HASH_LEN = 16;                    // 128 bits, in bytes
-    static constexpr uint32 ENCODED_HASH_LEN = 100;           // in chars
-    static constexpr uint32 DEFAULT_ITERATIONS = 10;          // determined by dice roll, guaranteed to be secure (not really)
-    static constexpr uint32 DEFAULT_MEMORY_COST = (1u << 17); // 2^17 kibibytes is 2^7 mebibytes is ~100MB
-    static constexpr uint32 PARALLELISM = 1;                  // we don't support threaded hashing
+    struct TC_COMMON_API Argon2
+    {
+        static constexpr uint32 HASH_LEN = 16;                    // 128 bits, in bytes
+        static constexpr uint32 ENCODED_HASH_LEN = 100;           // in chars
+        static constexpr uint32 DEFAULT_ITERATIONS = 10;          // determined by dice roll, guaranteed to be secure (not really)
+        static constexpr uint32 DEFAULT_MEMORY_COST = (1u << 17); // 2^17 kibibytes is 2^7 mebibytes is ~100MB
+        static constexpr uint32 PARALLELISM = 1;                  // we don't support threaded hashing
 
-    static Optional<std::string> Hash(std::string const& password, BigNumber const& salt, uint32 nIterations = DEFAULT_ITERATIONS, uint32 kibMemoryCost = DEFAULT_MEMORY_COST);
-    static bool Verify(std::string const& password, std::string const& hash);
-};
-}
+        static Optional<std::string> Hash(std::string const& password, BigNumber const& salt, uint32 nIterations = DEFAULT_ITERATIONS, uint32 kibMemoryCost = DEFAULT_MEMORY_COST);
+        static bool Verify(std::string const& password, std::string const& hash);
+    };
 }
 
 #endif

src/common/Cryptography/Authentication/AuthCrypt.cpp
@@ -16,58 +16,39 @@
  */
 
 #include "AuthCrypt.h"
-#include "Cryptography/HMACSHA1.h"
-#include "Cryptography/BigNumber.h"
+#include "BigNumber.h"
+#include "Errors.h"
+#include "HMAC.h"
 
 #include <cstring>
 
 AuthCrypt::AuthCrypt() :
-    _clientDecrypt(SHA_DIGEST_LENGTH), _serverEncrypt(SHA_DIGEST_LENGTH),
     _initialized(false)
 { }
 
-void AuthCrypt::Init(BigNumber* K)
+void AuthCrypt::Init(std::array<uint8, 40> const& K)
 {
-    uint8 ServerEncryptionKey[SEED_KEY_SIZE] = { 0xCC, 0x98, 0xAE, 0x04, 0xE8, 0x97, 0xEA, 0xCA, 0x12, 0xDD, 0xC0, 0x93, 0x42, 0x91, 0x53, 0x57 };
-    HmacHash serverEncryptHmac(SEED_KEY_SIZE, (uint8*)ServerEncryptionKey);
-    uint8 *encryptHash = serverEncryptHmac.ComputeHash(K);
-
-    uint8 ServerDecryptionKey[SEED_KEY_SIZE] = { 0xC2, 0xB3, 0x72, 0x3C, 0xC6, 0xAE, 0xD9, 0xB5, 0x34, 0x3C, 0x53, 0xEE, 0x2F, 0x43, 0x67, 0xCE };
-    HmacHash clientDecryptHmac(SEED_KEY_SIZE, (uint8*)ServerDecryptionKey);
-    uint8 *decryptHash = clientDecryptHmac.ComputeHash(K);
-
-    //ARC4 _serverDecrypt(encryptHash);
-    _clientDecrypt.Init(decryptHash);
-    _serverEncrypt.Init(encryptHash);
-    //ARC4 _clientEncrypt(decryptHash);
+    uint8 ServerEncryptionKey[] = { 0xCC, 0x98, 0xAE, 0x04, 0xE8, 0x97, 0xEA, 0xCA, 0x12, 0xDD, 0xC0, 0x93, 0x42, 0x91, 0x53, 0x57 };
+    _serverEncrypt.Init(Trinity::Crypto::HMAC_SHA1::GetDigestOf(ServerEncryptionKey, K));
+    uint8 ServerDecryptionKey[] = { 0xC2, 0xB3, 0x72, 0x3C, 0xC6, 0xAE, 0xD9, 0xB5, 0x34, 0x3C, 0x53, 0xEE, 0x2F, 0x43, 0x67, 0xCE };
+    _clientDecrypt.Init(Trinity::Crypto::HMAC_SHA1::GetDigestOf(ServerDecryptionKey, K));
 
     // Drop first 1024 bytes, as WoW uses ARC4-drop1024.
-    uint8 syncBuf[1024];
-    memset(syncBuf, 0, 1024);
-
-    _serverEncrypt.UpdateData(1024, syncBuf);
-    //_clientEncrypt.UpdateData(1024, syncBuf);
-
-    memset(syncBuf, 0, 1024);
-
-    //_serverDecrypt.UpdateData(1024, syncBuf);
-    _clientDecrypt.UpdateData(1024, syncBuf);
+    std::array<uint8, 1024> syncBuf;
+    _serverEncrypt.UpdateData(syncBuf);
+    _clientDecrypt.UpdateData(syncBuf);
 
     _initialized = true;
 }
 
 void AuthCrypt::DecryptRecv(uint8 *data, size_t len)
 {
-    if (!_initialized)
-        return;
-
-    _clientDecrypt.UpdateData(len, data);
+    ASSERT(_initialized);
+    _clientDecrypt.UpdateData(data, len);
 }
 
 void AuthCrypt::EncryptSend(uint8 *data, size_t len)
 {
-    if (!_initialized)
-        return;
-
-    _serverEncrypt.UpdateData(len, data);
+    ASSERT(_initialized);
+    _serverEncrypt.UpdateData(data, len);
 }

commit ad2641a8217ca72a59f847d66de14eb8835e342d

Core/Warden: Fix Warden reporting false positive checks
Issue introduced in d86c693c312bfde05be219ac3be98e2952732a89

src/server/game/Warden/WardenWin.cpp
@@ -391,7 +391,7 @@ void WardenWin::HandleData(ByteBuffer &buff)
                     continue;
                 }
 
-                std::vector<uint8> result = rs->Result.ToByteVector();
+                std::vector<uint8> result = rs->Result.ToByteVector(0, false);
                 if (memcmp(buff.contents() + buff.rpos(), result.data(), rd->Length) != 0)
                 {
                     TC_LOG_DEBUG("warden", "RESULT MEM_CHECK fail CheckId %u account Id %u", *itr, _session->GetAccountId());