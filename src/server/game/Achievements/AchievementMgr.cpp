jairoe05/tc_duplicate

Core/Entities: Second batch of removing implicit conversions of ObjectGuid to uint64

src/server/game/Achievements/AchievementMgr.cpp
@@ -868,7 +868,7 @@ void AchievementMgr<T>::SendAchievementEarned(AchievementEntry const* achievemen
         // broadcast realm first reached
         WorldPacket data(SMSG_SERVER_FIRST_ACHIEVEMENT, GetOwner()->GetName().size() + 1 + 8 + 4 + 4);
         data << GetOwner()->GetName();
-        data << uint64(GetOwner()->GetGUID());
+        data << GetOwner()->GetGUID();
         data << uint32(achievement->ID);
         data << uint32(0);                                  // 1=link supplied string as player name, 0=display plain string
         sWorld->SendGlobalMessage(&data);

Krigsgaldrnet/TrinityCore-3.3.5

(cherry picked from commit 034561792ca9a07d719961ea842af2118428816f)

src/server/game/Achievements/AchievementMgr.cpp
@@ -702,7 +702,7 @@ void AchievementMgr::SendAchievementEarned(AchievementEntry const* achievement) 
         // broadcast realm first reached
         WorldPacket data(SMSG_SERVER_FIRST_ACHIEVEMENT, GetPlayer()->GetName().size() + 1 + 8 + 4 + 4);
         data << GetPlayer()->GetName();
-        data << uint64(GetPlayer()->GetGUID());
+        data << GetPlayer()->GetGUID();
         data << uint32(achievement->ID);
 
         std::size_t linkTypePos = data.wpos();
1276676a335e55f6cc072ba238cde865f80797bc
Shauren <shauren.trinity@gmail.com>
2014-10-21 20:27:00 +0200

    Core/Entities: Second batch of removing implicit conversions of ObjectGuid to uint64
    (cherry picked from commit 034561792ca9a07d719961ea842af2118428816f)

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1437,7 +1437,8 @@ void WorldSession::HandleCharCustomizeCallback(std::shared_ptr<CharacterCustomiz
     }
 
     // character with this name already exist
-    if (ObjectGuid newGuid = sCharacterCache->GetCharacterGuidByName(customizeInfo->Name))
+    ObjectGuid newGuid = sCharacterCache->GetCharacterGuidByName(customizeInfo->Name);
+    if (!newGuid.IsEmpty())
     {
         if (newGuid != customizeInfo->Guid)
         {
