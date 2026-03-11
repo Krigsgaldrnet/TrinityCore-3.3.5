jairoe05/tc_duplicate

Core/Packets: CMSG_NAME_QUERY (QueryPlayerName) handler

src/server/game/Handlers/QueryHandler.cpp
@@ -64,15 +64,9 @@ void WorldSession::SendNameQueryOpcode(ObjectGuid guid)
     SendPacket(&data);
 }
 
-void WorldSession::HandleNameQueryOpcode(WorldPacket& recvData)
+void WorldSession::HandleNameQueryOpcode(WorldPackets::Character::QueryPlayerName& queryPlayerName)
 {
-    ObjectGuid guid;
-    recvData >> guid;
-
-    // This is disable by default to prevent lots of console spam
-    // TC_LOG_INFO("network", "HandleNameQueryOpcode %u", guid);
-
-    SendNameQueryOpcode(guid);
+    SendNameQueryOpcode(queryPlayerName.Player);
 }
 
 void WorldSession::HandleQueryTimeOpcode(WorldPacket & /*recvData*/)

src/server/game/Server/WorldSession.h
@@ -90,6 +90,7 @@ namespace WorldPackets
         class LogoutRequest;
         class LogoutCancel;
         class LoadingScreenNotify;
+        class QueryPlayerName;
     }
 
     namespace Guild
@@ -452,7 +453,7 @@ class WorldSession
         void HandleCharCreateCallback(PreparedQueryResult result, WorldPackets::Character::CharacterCreateInfo* createInfo);
         void HandlePlayerLoginOpcode(WorldPackets::Character::PlayerLogin& playerLogin);
         void HandleContinuePlayerLogin();
-        void HandleLoadScreenOpcode(WorldPacket& recvPacket);
+        void HandleLoadScreenOpcode(WorldPackets::Character::LoadingScreenNotify& loadingScreenNotify);
         void HandlePlayerLogin(LoginQueryHolder * holder);
         void HandleCharRenameOpcode(WorldPacket& recvData);
         void HandleCharRenameCallBack(PreparedQueryResult result, WorldPackets::Character::CharacterRenameInfo const* renameInfo);
@@ -568,7 +569,7 @@ class WorldSession
         void HandleMeetingStoneInfo(WorldPacket& recPacket);
         void HandleGameobjectReportUse(WorldPacket& recvPacket);
 
-        void HandleNameQueryOpcode(WorldPacket& recvPacket);
+        void HandleNameQueryOpcode(WorldPackets::Character::QueryPlayerName& queryPlayerName);
 
         void HandleQueryTimeOpcode(WorldPacket& recvPacket);
 

Krigsgaldrnet/TrinityCore-3.3.5

(cherry picked from commit ec7923c2e6f7778d82e8102d38cd54e0827fb840)

src/server/game/Handlers/QueryHandler.cpp
@@ -66,15 +66,9 @@ void WorldSession::SendNameQueryOpcode(ObjectGuid guid)
     SendPacket(&data);
 }
 
-void WorldSession::HandleNameQueryOpcode(WorldPacket& recvData)
+void WorldSession::HandleNameQueryOpcode(WorldPackets::Query::QueryPlayerName& queryPlayerName)
 {
-    ObjectGuid guid;
-    recvData >> guid;
-
-    // This is disable by default to prevent lots of console spam
-    // TC_LOG_INFO("network", "HandleNameQueryOpcode {}", guid);
-
-    SendNameQueryOpcode(guid);
+    SendNameQueryOpcode(queryPlayerName.Player);
 }
 
 void WorldSession::HandleQueryTimeOpcode(WorldPacket & /*recvData*/)

src/server/game/Server/WorldSession.h
@@ -239,6 +239,7 @@ namespace WorldPackets
     namespace Query
     {
         class QueryCreature;
+        class QueryPlayerName;
         class QueryGameObject;
         class QueryCorpseLocationFromClient;
         class QueryCorpseTransport;
@@ -802,7 +803,7 @@ class TC_GAME_API WorldSession
         void HandleGameObjectUseOpcode(WorldPacket& recPacket);
         void HandleGameobjectReportUse(WorldPacket& recvPacket);
 
-        void HandleNameQueryOpcode(WorldPacket& recvPacket);
+        void HandleNameQueryOpcode(WorldPackets::Query::QueryPlayerName& queryPlayerName);
 
         void HandleQueryTimeOpcode(WorldPacket& recvPacket);
 