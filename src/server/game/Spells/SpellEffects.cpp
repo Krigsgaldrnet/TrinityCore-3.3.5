jairoe05/tc_duplicate

Core/Spells: Fixed player spells with only spellpower scaling doing no damage

src/server/game/Spells/SpellEffects.cpp
@@ -561,7 +561,7 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
             }
         }
 
-        if (m_originalCaster && damage > 0 && apply_direct_bonus)
+        if (m_originalCaster && apply_direct_bonus)
         {
             damage = m_originalCaster->SpellDamageBonusDone(unitTarget, m_spellInfo, (uint32)damage, SPELL_DIRECT_DAMAGE);
             damage = unitTarget->SpellDamageBonusTaken(m_originalCaster, m_spellInfo, (uint32)damage, SPELL_DIRECT_DAMAGE);

Krigsgaldrnet/TrinityCore-3.3.5

(cherry picked from commit 19528f54a501f4f137d0a333b8d79d754590f84c)

src/server/game/Spells/SpellEffects.cpp
@@ -675,7 +675,7 @@ void Spell::EffectSchoolDMG()
             }
         }
 
-        if (unitCaster && damage > 0 && apply_direct_bonus)
+        if (unitCaster && apply_direct_bonus)
         {
             damage = unitCaster->SpellDamageBonusDone(unitTarget, m_spellInfo, (uint32)damage, SPELL_DIRECT_DAMAGE, *effectInfo, { });
             damage = unitTarget->SpellDamageBonusTaken(unitCaster, m_spellInfo, (uint32)damage, SPELL_DIRECT_DAMAGE);